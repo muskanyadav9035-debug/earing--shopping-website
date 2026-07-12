6f0fc37d11ab65f503a80956c7b354275ed0f520
muskanyadav9035-debug <muskanyadav9035@gmail.com>
2026-07-12 17:45:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,65 @@
-let cart = 0;
-let total = 0;
-let items = {};
+
+ let cart = Number(localStorage.getItem("cart")) || 0;
+let total = Number(localStorage.getItem("total")) || 0;
+
+let items = JSON.parse(localStorage.getItem("items")) || {};
+
 
 function addToCart(name, price) {
+
     cart++;
     total += price;
 
     document.getElementById("cartCount").innerText = cart;
     document.getElementById("total").innerText = total;
 
+
     if (items[name]) {
         items[name].quantity++;
         items[name].amount += price;
-    } else {
+    } 
+    else {
         items[name] = {
             quantity: 1,
             amount: price
         };
     }
 
+
+    localStorage.setItem("cart", cart);
+    localStorage.setItem("total", total);
+    localStorage.setItem("items", JSON.stringify(items));
+
     updateCart();
 }
 
+
 function updateCart() {
+
     let cartList = document.getElementById("cartList");
+
+    if(!cartList) return;
+
     cartList.innerHTML = "";
 
+
     for (let name in items) {
+
         let li = document.createElement("li");
+
         li.innerText =
-            `${name} x${items[name].quantity} - ₹${items[name].amount}`;
+        `${name} x${items[name].quantity} - ₹${items[name].amount}`;
+
         cartList.appendChild(li);
     }
 }
 
+
 function buyNow() {
-    localStorage.setItem("cart", cart);
-    localStorage.setItem("total", total);
 
     window.location.href = "bill.html";
+
 }
+
+
+updateCart();

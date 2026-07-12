e1c7de35aaf4abe4cc5c74b8f4fe14e9eae33213
muskanyadav9035-debug <muskanyadav9035@gmail.com>
2026-07-12 17:22:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 let cart = 0;
 let total = 0;
 
-function addToCart(name,price){
+function addToCart(name, price) {
     cart++;
     total += price;
 
     document.getElementById("cartCount").innerText = cart;
     document.getElementById("total").innerText = total;
 
-    alert(name + " added to cart!");
+    let li = document.createElement("li");
+    li.innerText = name + " - ₹" + price;
+    document.getElementById("cartList").appendChild(li);
 }
 
-function buyNow(){
+function buyNow() {
     alert("Thank you for shopping!\nTotal Amount: ₹" + total);
 }
-
-document.getElementById("search").addEventListener("keyup", function(){
-    let value = this.value.toLowerCase();
-    let products = document.querySelectorAll(".product");
-
-    products.forEach(function(product){
-        let name = product.dataset.name.toLowerCase();
-
-        if(name.includes(value)){
-            product.style.display = "block";
-        }
-        else{
-            product.style.display = "none";
-        }
-    });
-});
